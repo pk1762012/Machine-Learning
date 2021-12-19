c5fd62918c67f5e6d47c77cab8e6254e35d8d244
pk1762012 <92876246+pk1762012@users.noreply.github.com>
2021-12-19 21:04:55 +0530

    Decision tree code - criterion = entropy

---
+++ Decision Tree.py
@@ -0,0 +1,49 @@
+# Load libraries
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split  # Import train_test_split function
+from sklearn import metrics  # Import scikit-learn metrics module for accuracy calculation
+
+from sklearn.tree import export_graphviz
+from sklearn import tree
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+
+import collections
+from sklearn.tree import plot_tree
+
+
+
+# col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+# load dataset
+file = pd.read_csv("diabetes.csv")
+
+feature_cols = ['Pregnancies', 'Insulin', 'BMI', 'Age', 'Glucose', 'BloodPressure', 'DiabetesPedigreeFunction']
+X = file[feature_cols]
+y = file.Outcome
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# Create Decision Tree classifier object
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+
+# Train Decision Tree Classifier
+clf = clf.fit(X_train, y_train)
+
+# Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+
+dot_data = StringIO()
+
+export_graphviz(clf, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True, feature_names=feature_cols, class_names=['0', '1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_jpeg('diabetes.png')
+Image(graph.create_jpeg())
